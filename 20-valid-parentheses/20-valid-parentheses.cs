cc0a8234937b0774e53d4312cd2bd0016d02ec73
Nick Kuria <nkuria01@gmail.com>
2022-06-22 15:56:48 +0300

    Time: 103 ms (50.98%), Space: 36.9 MB (34.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cs
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -1,10 +1,7 @@
 public class Solution {
     public bool IsValid(string s) {
-           // Stack new = new Stack();
         Stack my_stack = new Stack();
-        
-        
-        
+ 
         for(int i = 0; i < s.Length; i++) {
             if( s[i].Equals( '(' ) || s[i].Equals( '{' ) || s[i].Equals( '[' )) my_stack.Push(s[i]);
             else if(s[i].Equals( ')' )) {
@@ -12,7 +9,6 @@ public class Solution {
                 else my_stack.Pop();
             } else if(s[i].Equals( '}' )) {
                 if(my_stack.Count == 0  ||  !my_stack.Peek().Equals( '{' ) ) return false;
-                
                 else my_stack.Pop();
             } else if(s[i].Equals( ']' )) {
                 if(my_stack.Count == 0 || !my_stack.Peek().Equals( '[' ) ) return false;
