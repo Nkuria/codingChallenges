4b59c1d354b566b78e9af32ea7356041d1dffccb
Nick Kuria <nkuria01@gmail.com>
2022-06-28 17:13:18 +0300

    Time: 159 ms (54.73%), Space: 40.2 MB (39.12%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cs
+++ 724-find-pivot-index/724-find-pivot-index.cs
@@ -2,13 +2,11 @@ public class Solution {
     public int PivotIndex(int[] nums) {
         int sum = 0;
         int leftSum = 0;
-        foreach(int x in nums) sum = sum + x;   
-        
+        foreach(int x in nums) sum +=  x;   
         for(int i = 0; i < nums.Length; i++) {
             if(leftSum == sum -leftSum - nums[i] ) return i;
              leftSum += nums[i];
         }
-        
         return -1;
     }
 }
