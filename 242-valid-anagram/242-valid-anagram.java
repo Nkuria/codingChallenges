729345ed08c80ae24c081f87a881b2c856f07573
Nick Kuria <nkuria01@gmail.com>
2023-03-13 11:43:53 +0300

    Time: 3 ms (82.74%), Space: 42.7 MB (43.28%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,25 +4,38 @@ class Solution {
             return false;
         }
         
-        HashMap<Character, Integer> mp = new HashMap<>();
+        // HashMap<Character, Integer> mp = new HashMap<>();
         
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(mp.containsKey(c)) {
-                mp.put(c, mp.get(c) + 1);
-            }else {
-                mp.put(c, 1);
-            }
-        }
+        // for(int i = 0; i < s.length(); i++) {
+        //     char c = s.charAt(i);
+        //     if(mp.containsKey(c)) {
+        //         mp.put(c, mp.get(c) + 1);
+        //     }else {
+        //         mp.put(c, 1);
+        //     }
+        // }
         
-        for(int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            if(mp.containsKey(c) && mp.get(c) > 0) {
-                mp.put(c, mp.get(c) - 1);
-            }else {
+        // for(int i = 0; i < t.length(); i++) {
+        //     char c = t.charAt(i);
+        //     if(mp.containsKey(c) && mp.get(c) > 0) {
+        //         mp.put(c, mp.get(c) - 1);
+        //     }else {
+        //         return false;
+        //     }
+        // }
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+        
+        for(int i =0; i < ss.length; i++) {
+            if(ss[i] != tt[i]) {
                 return false;
+                
             }
         }
+        
         return true;
     }
 }
