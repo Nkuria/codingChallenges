c7afdfd5e4e6356bc15a48cd8fad935f2e0cf130
Nick Kuria <nkuria01@gmail.com>
2023-03-20 13:54:50 +0300

    Time: 1 ms (100.00%), Space: 51.9 MB (29.58%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > currentSum + nums[i]) {
+                currentSum = nums[i];
+            }else {
+                currentSum = nums[i] + currentSum;
+            }
+
+            if(currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+}
