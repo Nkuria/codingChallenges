d1ed60c9761fd9bfe37da9e5efa3226fdbbe4f8b
Nick Kuria <nkuria01@gmail.com>
2023-04-17 09:23:01 +0300

    Time: 1 ms (97.11%), Space: 49.4 MB (67.58%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0;
+        int high = s.length -1;
+        while(low < high) {
+            char temp = s[high];
+            s[high] = s[low];
+            s[low] = temp;
+            high--;
+            low++;
+        }
+        
+        
+    }
+}
